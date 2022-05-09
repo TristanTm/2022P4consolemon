1958abcd3c7872717b31431c19c6dae3a5405802
TristanTM <33825@ma-web.nl>
2022-05-09 10:34:11 +0200

    yay

---
+++ ConsoleMon/ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class ConsoleMon
+    {
+        public string Name;
+        public int HP;
+        public int energy;
+
+
+        public void TakeDamage(int damage)
+        {
+            HP = HP - damage; 
+        }
+
+        public void DepleteEnergy(int energy)
+        {
+            this.energy = this.energy - energy;
+        }
+    }
+
+
+}
---
+++ ConsoleMon/ConsoleMon/Skill.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class Skill
+    {
+        public string skillName;
+        public int damage;
+        public int energyCost;
+
+
+
+        internal void UseOn(ConsoleMon caster, ConsoleMon target)
+        {
+            caster.DepleteEnergy(energyCost);
+            target.TakeDamage(damage);
+        }
+    }
+
+ 
+}
